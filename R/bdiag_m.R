92457f5bf95e9a15851fb95451b1f6672f21db16
Joaquin Martínez-Minaya <jomarminaya@gmail.com>
2020-08-26 18:32:07 +0200

    new function to deal with sparse matrix

---
+++ R/bdiag_m.R
@@ -0,0 +1,20 @@
+##' Fast version of Matrix :: .bdiag() -- for the case of *many*  (k x k) matrices:
+##' @param lmat list(<mat1>, <mat2>, ....., <mat_N>)  where each mat_j is a  k x k 'matrix'
+##' @return a sparse (N*k x N*k) matrix of class  \code{"\linkS4class{dgCMatrix}"}.
+bdiag_m <- function(lmat) {
+  ## Copyright (C) 2016 Martin Maechler, ETH Zurich
+  if(!length(lmat)) return(new("dgCMatrix"))
+  stopifnot(is.list(lmat), is.matrix(lmat[[1]]),
+            (k <- (d <- dim(lmat[[1]]))[1]) == d[2], # k x k
+            all(vapply(lmat, dim, integer(2)) == k)) # all of them
+  N <- length(lmat)
+  if(N * k > .Machine$integer.max)
+    stop("resulting matrix too large; would be  M x M, with M=", N*k)
+  M <- as.integer(N * k)
+  ## result: an   M x M  matrix
+  new("dgCMatrix", Dim = c(M,M),
+      ## 'i :' maybe there's a faster way (w/o matrix indexing), but elegant?
+      i = as.vector(matrix(0L:(M-1L), nrow=k)[, rep(seq_len(N), each=k)]),
+      p = k * 0L:M,
+      x = as.double(unlist(lmat, recursive=FALSE, use.names=FALSE)))
+}
