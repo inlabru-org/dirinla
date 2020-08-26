e8a1e6932f21b5e1672eef83ad852671fbc3bb82
Joaquin Martínez-Minaya <jomarminaya@gmail.com>
2020-08-26 09:05:34 +0200

    improving computational time for Hessian matrix

--- R/H0_matrix_eta1.R
+++ R/H0_matrix_eta1.R
@@ -1,20 +1,25 @@
 #' Computing expected Hessian in eta
 #'
-#' `H0_matrix_eta` computes the expected Hessian in eta of -loglikelihood
+#' `H0_matrix_x` computes the expected Hessian in eta of -loglikelihood
 #'
 #' @param A Matrix which links eta with the latent field, i.e., eta = A x.
 #' @param x Vector with the elements of the latent field, i.e., eta = A x.
 #' @param d Dimension
+#' @param cores Number of cores for parallel computation. The package parallel is used.
 #'
 #' @return Expected Hessian in eta.
-#'
-#' @author Joaquín Martínez-Minaya <\email{joaquin.martinez-minaya@@uv.es}>
+#' @import parallel
+#' @author Joaquín Martínez-Minaya <\email{jomarminaya@@gmail.com}>
 
-H0_matrix_eta_x <- function(A, x, d) {
+H0_matrix_eta_x <- function(A, x, d, cores) {
   eta <- Matrix::Matrix(as.numeric(A %*% x), ncol = d, byrow = TRUE)
-  eta_list <-  split(eta, rep(1:nrow(eta), each = ncol(eta)))
+  a1 <- apply(eta, 1, H0_matrix_eta1, d)
+  parallel::mclapply(a1, "[[", 1, mc.cores = cores)
 
-  lapply(eta_list, H0_matrix_eta1, d = 4)
+
+  # eta_list <- split(eta, rep(1:nrow(eta), each = ncol(eta)))
+  # a <- parallel::mclapply(eta_list, H0_matrix_eta1, d, mc.cores = 4)
+
 }
 
 
@@ -36,6 +41,6 @@ H0_matrix_eta1 <- function(eta, d) {
   }
 
   # t(A) %*% H0 %*% A
-  -H0
+  list(-H0)
 }
 
